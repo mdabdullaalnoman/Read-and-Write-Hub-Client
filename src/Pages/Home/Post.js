e23846182a481310a953cfbfd006c58277bb384c
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-04-05 00:26:19 +0600

    home page post section desing

--- src/Pages/Home/Post.js
+++ src/Pages/Home/Post.js
@@ -11,19 +11,19 @@ const Post = () => {
             .then(err => console.log(err))
     }, []);
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-8 lg:gap-10 px-5  md:px-5 lg:px-10 bg-slate-300 py-20 md:py-25">
             {
                 post.map(post =>
                     <div key={post.id}>
-                        <div className="blog-cart-warp ">
-                            <div className="blog-cart-img">
-                                <img src={post.imgURL} alt={post.title} />
+                        <div className="blog-cart-warp bg-white">
+                            <div className="blog-cart-img ">
+                                <img className='w-full' src={post.imgURL} alt={post.title} />
                             </div>
-                            <div className="blog-content">
-                                <p>{post.date}</p>
-                                <h3>{post.title}</h3>
-                                <p>{post.description}</p>
-                                <p>{post.category}</p>
+                            <div className="blog-content  p-4">
+                                <p className='text-gray-500 font-semibold'>{post.date}</p>
+                                <h3 className='font-serif text-2xl font-large py-5'>{post.title}</h3>
+                                <p className='font-sans text-gray-500'>{post.description}</p>
+                                <p className='pt-5 text-blue-600 text-lg font-semibold'>{post.category}</p>
                             </div>
                         </div>
                     </div>
