ca6d12659b442161d71c06356253549974d64ea4
Fahim-Faysal <faysalsiyam@gmail.com>
2022-04-04 23:58:34 +0600

    update

--- src/Authentication/Register/Register.js
+++ src/Authentication/Register/Register.js
@@ -31,13 +31,13 @@ const Register = () => {
 
                     <div className="text-center my-6">
 
-                        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                        {/* <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             <strong className="font-bold">Error!</strong>
                             <span className="block sm:inline">"error"</span>
                             <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
                                 <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
                             </span>
-                        </div>
+                        </div> */}
 
                         <h1 className="text-3xl font-semibold text-gray-700">Register</h1>
                         <p className="text-gray-500">Sign up to access your account</p>
@@ -46,7 +46,7 @@ const Register = () => {
                     <div className="m-6">
                         <form onSubmit={handleSubmit} className="mb-4">
                             <div className="mb-6">
-                                <label for="name" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Your Name</label>
+                                <label htmlFor="name" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Your Name</label>
                                 <input
                                     type="text"
                                     name="name"
@@ -56,7 +56,7 @@ const Register = () => {
                                     className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                             </div>
                             <div className="mb-6">
-                                <label for="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                                <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
                                 <input
                                     type="email"
                                     name="email"
@@ -67,7 +67,7 @@ const Register = () => {
                             </div>
                             <div className="mb-6">
                                 <div className="flex justify-between mb-2">
-                                    <label for="password" className="text-sm text-gray-600 dark:text-gray-400">Password</label>
+                                    <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-400">Password</label>
                                 </div>
                                 <input
                                     type="password"
