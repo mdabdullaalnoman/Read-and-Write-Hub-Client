5ec795107a6074cf0bd74158421edaaf0c1e8abf
Fahim-Faysal <faysalsiyam@gmail.com>
2022-04-04 12:12:43 +0600

    add-blog form

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div>
-            <h1>this is not found</h1>
+            <h1 className='text-lime-700 flex justify-center text-4xl'>Write Your Blog Here</h1>
+            <div className='flex justify-center'>
+                <form className="w-4/5 lg:w-2/5 md:w-3/5">
+                    <div className="flex flex-wrap -mx-3  mb-4">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                Title
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Title" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3  mb-4">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                Author Name
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Author Name" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3  mb-4">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                Image Link
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Image URL Link" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3  mb-4">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                Description
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Description" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap -mx-3  mb-4">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                Write Blog
+                            </label>
+                            <textarea cols="30" rows="10" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Write Blog" />
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
