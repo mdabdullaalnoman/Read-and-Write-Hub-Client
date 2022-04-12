5bac591799015350052e1c007b9bb6178c205dae
Fahim-Faysal <faysalsiyam@gmail.com>
2022-04-12 11:40:08 +0600

    add-detailed page

--- src/Pages/ViewPost/ViewPost.js
+++ src/Pages/ViewPost/ViewPost.js
@@ -5,6 +5,7 @@ const ViewPost = () => {
     const { id } = useParams();
 
     const [blog, setBlog] = useState({});
+    console.log(blog);
     useEffect(() => {
         const url = ` https://writehubs.herokuapp.com/blogs/${id}`
         fetch(url)
@@ -15,18 +16,19 @@ const ViewPost = () => {
     }, [id])
     return (
         <div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-8 lg:gap-10 px-5  md:px-5 lg:px-10 bg-slate-300 py-20 md:py-25">
-
-                <div>
-                    <div className="blog-cart-warp bg-white">
-                        <div className="blog-cart-img ">
-                            <img className='w-full' src={blog.img} alt={blog.title} />
-                        </div>
-                        <div className="blog-content  p-4">
-                            <p className='text-gray-500 font-semibold'>{blog.date}</p>
-                            <p className='font-sans text-gray-500'>{blog.blog}</p>
-                            <p className='pt-5 text-blue-600 text-lg font-semibold'>{blog.category}</p>
-                        </div>
+            <div className='flex flex-col justify-center items-center my-12'>
+                <p className='text-5xl font-medium my-2.5'>{blog?.title}</p>
+                <p className='text-lg my-2.5'>{blog?.category}</p>
+                <img style={{ width: "60%" }} src={blog?.img} alt="" />
+                <p className='my-2.5 font-light'>{blog?.blog}</p>
+            </div>
+            <div className=' ml-28 my-12'>
+                <p className='text-4xl font-semibold my-9'>1 Comments</p>
+                <div className='w-1/2 flex justify-between items-center'>
+                    <img style={{ width: '100px' }} src='https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1649742284~hmac=da511d3d1e48ccdea0b125ae03d58f4e' alt="" />
+                    <div>
+                        <p className='text-2xl font-medium'>Fahim Faysal Siyam</p>
+                        <p>This is a very useful post and I am keen to learn about this more.</p>
                     </div>
                 </div>
             </div>
