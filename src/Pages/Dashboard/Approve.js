60cc02c50d48d4fb89378d2d585de306204d8e25
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-04-24 02:25:21 +0600

    admin dashboard responsive

--- src/Pages/Dashboard/Approve.js
+++ src/Pages/Dashboard/Approve.js
@@ -8,47 +8,47 @@ const Approve = () => {
     return (
 
         <div>
-            <div class="relative overflow-x-auto shadow-md">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="relative overflow-x-auto shadow-md  p-5">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 ID
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Name
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Created As
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Status
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Approve
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Delete
                             </th>
 
                         </tr>
                     </thead>
                     <tbody>
-                        {blogs.map(blog => <tr class="bg-white dark:bg-gray-800">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                        {blogs.map(blog => <tr key={blog._id} className="bg-white dark:bg-gray-800">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
                                 {blog.title}
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 {blog.topic}
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Accessories
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $99
                             </td>
 
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 <button onClick={() => confirmBlog(blog._id)} className="px-4 py-1 text-sm text-white bg-blue-400 rounded">
                                     {
                                         blog.status ? 'Approve' : 'Pending'
@@ -56,7 +56,7 @@ const Approve = () => {
                                 </button>
                             </td>
 
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 <button onClick={() => deletBlog(blog._id)} className="px-4 py-1 text-sm text-white bg-red-400 rounded">Delete</button>
                             </td>
 
@@ -68,66 +68,7 @@ const Approve = () => {
             </div>
         </div>
 
-        // <div className="overflow-x-scrool">
-        //     <div class="container flex justify-center ">
-        //         <div class="w-full">
-        //             <div class="border-b border-gray-200 shadow">
-        //                 <table>
-        //                     <thead class="bg-gray-50">
-        //                         <tr>
-        //                             <th class="px-6 py-2 text-xs text-gray-500">
-        //                                 ID
-        //                             </th>
-        //                             <th class="px-6 py-2 text-xs text-gray-500">
-        //                                 Name
-        //                             </th>
-
-        //                             <th class="px-6 py-2 text-xs text-gray-500">
-        //                                 Created_at
-        //                             </th>
-        //                             <th class="px-6 py-2 text-xs text-gray-500">
-        //                                 status
-        //                             </th>
-
-        //                         </tr>
-        //                     </thead>
-        //                     <tbody class="bg-white">
-        //                         {
-        //                             blogs.map(blog => (<tr key={blog._id} class="whitespace-nowrap">
-        //                                 <td class="px-6 py-4 text-sm text-gray-500">
-        //                                     {blog.title}
-        //                                 </td>
-        //                                 <td class="px-6 py-4">
-        //                                     <div class="text-sm text-gray-900">
-        //                                         {blog.topic}
-        //                                     </div>
-        //                                 </td>
-
-        //                                 <td class="px-6 py-4 text-sm text-gray-500">
-        //                                     2021-1-12
-        //                                 </td>
-        //                                 <td class="px-6 py-4 text-sm text-gray-500">
-        //                                     {
-        //                                         blog.status ? 'Approve' : 'Pending'
-        //                                     }
-        //                                 </td>
-        //                                 <td class="px-6 py-4">
-        //                                     <button onClick={() => confirmBlog(blog._id)} to="/" class="px-4 py-1 text-sm text-white bg-blue-400 rounded">approve  </button>
-        //                                 </td>
-        //                                 <td class="px-6 py-4">
-        //                                     <button onClick={() => deletBlog(blog._id)} class="px-4 py-1 text-sm text-white bg-red-400 rounded">Delete  </button>
-        //                                 </td>
-        //                             </tr>
-
-
-        //                             ))
-        //                         }
-        //                     </tbody>
-        //                 </table>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
+      
     );
 };
 
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaPinterest } from 'react-icons/fa';
 import { Link, Outlet } from 'react-router-dom';
-import useAuth from '../../Hooks/useAuth';
-
+import DashboardNav from './DashboardNav';
 
 const Dashboard = () => {
-    const {user}= useAuth()
-    console.log(user)
+    // for dashboard navbar toggle
+    const [dashToggle, setDashToggle] = useState(true);
+    const handleDashToggle = () => {
+        console.log(dashToggle);
+        if (dashToggle === false) {
+            setDashToggle(true)
+        }
+        else {
+            setDashToggle(false)
+        }
+    }
+
     return (
         <div>
             <div className="dashboard grid grid-cols-6">
-                <div className="sidebar flex flex-col min-h-screen bg-violet-900 text-white p-5">
-                    <Link to="/dashboard/approve">Approve</Link>
-                    <Link to="/dashboard/chart">Chart </Link>
-                    <Link to="/dashboard/myBlogs">My Blogs</Link>
-                    <Link to="/dashboard/setting">Setting</Link>
+                <div className={` sidebar flex flex-col min-h-screen bg-darker text-white border-r-2 border-primaryDarker p-5  `} >
+                    <Link to="/dashboard/approve" className='flex  items-center	'><span className='pr-3'><FaPinterest /></span>Approve</Link>
+                    <Link to="/dashboard/chart" className='flex  items-center'><span className='pr-3'><FaPinterest /></span>Chart </Link>
+                    <Link to="/dashboard/myBlogs" className='flex  items-center	'><span className='pr-3'><FaPinterest /></span>My Blogs</Link>
+                    <Link to="/dashboard/setting" className='flex  items-center	'><span className='pr-3'><FaPinterest /></span>Setting</Link>
                 </div>
-                <div className="col-span-5">
-                    <Outlet />
+
+                <div className={`col-span-5`}>
+                    <DashboardNav dashToggleF={handleDashToggle} dashToggle={dashToggle} />
+                    <div className='bg-darker'>
+                        <Outlet />
+                    </div>
                 </div>
             </div>
         </div>
