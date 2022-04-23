2cf2b4a7543e4c0583e9bdc3d051446459ee962f
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-04-23 23:20:02 +0600

    addmin ui

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import Footer from '../../SharedComponent/Footer';
-import Header from '../../SharedComponent/Header';
-
 const Dashboard = () => {
     return (
         <div>
-            <div>
-                <Header />
-                <div className="dashboard grid grid-cols-[300px_minmax(900px,_1fr)]">
-                    <div className="sidebar flex flex-col min-h-screen bg-violet-900 text-white p-5">
-                        <Link to="/dashboard/approve">Approve</Link>
-                        <Link to="/dashboard/chart">Chart </Link>
-                        <Link to="/dashboard/myBlogs">My Blogs</Link>
-                        <Link to="/dashboard/setting">Setting</Link>
-                    </div>
+            <div className="dashboard grid grid-cols-6">
+                <div className="sidebar flex flex-col min-h-screen bg-violet-900 text-white p-5">
+                    <Link to="/dashboard/approve">Approve</Link>
+                    <Link to="/dashboard/chart">Chart </Link>
+                    <Link to="/dashboard/myBlogs">My Blogs</Link>
+                    <Link to="/dashboard/setting">Setting</Link>
+                </div>
+                <div className="col-span-5">
                     <Outlet />
                 </div>
-                <Footer />
             </div>
         </div>
+
     );
 };
 
