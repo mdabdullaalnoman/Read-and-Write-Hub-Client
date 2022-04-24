7923de7826797ebcc6b31cf6039580057d3c61c7
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-04-25 01:16:37 +0600

    done

--- src/Pages/Dashboard/MyBlogs.js
+++ src/Pages/Dashboard/MyBlogs.js
@@ -3,9 +3,10 @@ import useAuth from '../../Hooks/useAuth';
 import useBlogs from '../../Hooks/useBlogs';
 
 const MyBlogs = () => {
-    const{user}= useAuth()
-    const {blogs}= useBlogs()
-    const [myBlogs, setMyBlogs]= useState([])
+    const { user } = useAuth();
+    const [myBlogs, setMyBlogs] = useState([]);
+    const { blogs, confirmBlog, deletBlog } = useBlogs();
+
     useEffect(() => {
         if (user.email) {
             const myBlogsData = blogs.filter(blog => blog.email === user.email);
@@ -14,10 +15,66 @@ const MyBlogs = () => {
     }, [blogs, user.email])
     return (
         <div>
-            my blogs
-            {
-                myBlogs.length
-            }
+            <div>
+                <div className="relative overflow-x-auto shadow-md  p-5">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    ID
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Created As
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Status
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Approve
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Delete
+                                </th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {myBlogs.map(blog => <tr key={blog._id} className="bg-white dark:bg-gray-800">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                    {blog.title}
+                                </th>
+                                <td className="px-6 py-4">
+                                    {blog.topic}
+                                </td>
+                                <td className="px-6 py-4">
+                                    Accessories
+                                </td>
+                                <td className="px-6 py-4">
+                                    $99
+                                </td>
+
+                                <td className="px-6 py-4">
+                                    <button onClick={() => confirmBlog(blog._id)} className="px-4 py-1 text-sm text-white bg-blue-400 rounded">
+                                        {
+                                            blog.status ? 'Approve' : 'Pending'
+                                        }
+                                    </button>
+                                </td>
+
+                                <td className="px-6 py-4">
+                                    <button onClick={() => deletBlog(blog._id)} className="px-4 py-1 text-sm text-white bg-red-400 rounded">Delete</button>
+                                </td>
+
+                            </tr>)
+
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       fontFamily: {
         Libre: ['Libre Baskerville', 'serif'],
         Nanum: ['Nanum Gothic', 'sans-serif'],
         Open: ['Open Sans', 'sans-serif'],
         Inter: ['Inter', 'sans-serif'],
-      }
-=======
+      },
       colors: {
         'darker': '#12263f',
         'darks': '#152e4d',
         'whiter': '#ecfeff',
         'primaryDarker': '#155e75'
       },
->>>>>>> 6a3cd78f3b68972cebcffe032b98eb404b98fb49
     },
   },
   plugins: [
